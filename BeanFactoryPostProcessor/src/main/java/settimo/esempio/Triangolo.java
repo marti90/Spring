a5c1ab63f2bfd1edf726bce5723111cc4c31b93d
marti90 <debernardi.martina@gmail.com>
2016-11-03 16:54:41 +0100

    Bean Factory Post Processor

---
+++ BeanFactoryPostProcessor/src/main/java/settimo/esempio/Triangolo.java
@@ -0,0 +1,60 @@
+package settimo.esempio;
+
+public class Triangolo {
+	
+	  private Punto puntoA;
+	  private Punto puntoB;
+	  private Punto puntoC;
+	
+	  public Triangolo() {
+			
+		}
+	  
+	
+
+	public void stampaVertici(){
+		System.out.println("coordinato dell A:"+ puntoA.getX()+"-"+puntoA.getY());
+ 		System.out.println("coordinato dell B:"+ puntoB.getX()+"-"+puntoB.getY());
+		System.out.println("coordinato dell C:"+ puntoC.getX()+"-"+puntoC.getY());
+	
+	}
+
+
+
+	public Punto getPuntoA() {
+		return puntoA;
+	}
+
+
+
+	public void setPuntoA(Punto puntoA) {
+		this.puntoA = puntoA;
+	}
+
+
+
+	public Punto getPuntoB() {
+		return puntoB;
+	}
+
+
+
+	public void setPuntoB(Punto puntoB) {
+		this.puntoB = puntoB;
+	}
+
+
+
+	public Punto getPuntoC() {
+		return puntoC;
+	}
+
+
+
+	public void setPuntoC(Punto puntoC) {
+		this.puntoC = puntoC;
+	}
+
+	
+
+}
