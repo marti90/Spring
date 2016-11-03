1a908fb84298f3a1a9d986c7b431a1a8c8011b89
marti90 <debernardi.martina@gmail.com>
2016-11-03 16:59:57 +0100

    Iniezione degli Oggetti

---
+++ IniezioneObject/src/main/java/secondo/esempio/Esempio.java
@@ -0,0 +1,20 @@
+package secondo.esempio;
+
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Esempio {
+
+	public static void main(String[] args) {
+		
+	
+		 ApplicationContext context=new ClassPathXmlApplicationContext("spring.xml");
+		 
+		   Triangolo t=(Triangolo) context.getBean("triangolo");
+		   
+		   t.stampaVertici();
+		   
+	}
+
+}
